e83bfd45c16b0ddcbcce8139576c0035bff4367d
Luke1760 <kjd1760@gmail.com>
2020-12-06 14:13:04 +0800

    feature: Sign up

---
+++ app/controllers/api/v1/registrations_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::RegistrationsController < Devise::RegistrationsController
+
+  # sign up feature
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: {
+        message: 'Signed Up Successfully',
+        is_success: true,
+        data: {
+          user: user
+        }
+      }, status: :ok
+      else
+        render json: {
+          message: 'Something Wrong',
+          is_success: false,
+          data: {}
+        }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
