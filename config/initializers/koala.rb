2e2b778a6d28a61066645e594ca1bdf7fb52959e
Luke1760 <kjd1760@gmail.com>
2020-12-20 13:48:42 +0800

    create: facebook application

---
+++ config/initializers/koala.rb
@@ -0,0 +1,6 @@
+Koala.configure do |config|
+  config.app_id = "1906391856166608"
+  config.app_secret = "72618ad97a82691a642059471ed81943"
+  # See Koala::Configuration for more options, including details on how to send requests through
+  # your own proxy servers.
+end
