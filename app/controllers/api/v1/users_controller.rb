08e3a8387c522786817e211ab915fd38b69baa2f
Luke1760 <kjd1760@gmail.com>
2020-12-20 16:15:19 +0800

    refactor: facebook method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::UsersController < ApplicationController
   
   def facebook
-    if params[:facebook_authentication_token]
-      graph = Koala::Facebook::API.new(params: [:facebook_authentication_token])
+    if params[:facebook_access_token]
+      graph = Koala::Facebook::API.new(params:[:facebook_access_token])
       user_data = graph.get_object("me?fields=name,email,id,picture")
 
-      user = User.find_by(email: user_data[:email])
+      user = User.find_by(email: user_data["email"])
       if user
         user.generate_new_authentication_token
         json_response "User Information", true, {user: user}, :ok
@@ -16,12 +16,13 @@ class Api::V1::UsersController < ApplicationController
                         image: user_data["picture"]["data"]["url"],
                         passeord: Devise.friendly_token[0,20])
 
-        user.generate_new_authentication_toke = User.generate_unique_secure_token
-
-      if user.save
-        json_response "Login Facebook Successfully", true, {user: user}, :ok
-      else
-        json_response user.errors, false, {}, :unprocessable_entity
+        user.authentication_token = User.generate_unique_secure_token
+        
+        if user.save
+          json_response "Login Facebook Successfully", true, {user: user}, :ok
+        else
+          json_response user.errors, false, {}, :unprocessable_entity
+        end
       end
     else
       json_response "Missing facebook access token", false, {}, :unprocessable_entity
